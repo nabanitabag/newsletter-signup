762ec0d6787704c8478dc9622b2f687a34c0627d
Nabanita <ashley.makruz@gmail.com>
2021-02-19 18:48:14 +0530

    add static files for exprss server

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", function (req,res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
 app.listen(3000,function() {
   console.log("running on 3000");
-})
+});
