0869522a1da57e1e72bf336784293e8d3f1990f1
Nabanita <ashley.makruz@gmail.com>
2021-02-19 22:11:15 +0530

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,65 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-
+const https = require('https');
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req,res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen(3000,function() {
+app.post("/", function (req, res) {
+ const firstname = req.body.fname;
+ const lastname = req.body.lname;
+ const email = req.body.email;
+
+const data = {
+  members: [
+    {
+      email_address: email,
+      status: "subscribed",
+      merge_fields:{
+        FNAME: firstname,
+        LNAME: lastname
+      }
+    }
+  ]
+};
+
+const jsonData = JSON.stringify(data);
+const url = 'https://us1.api.mailchimp.com/3.0/lists/d2f55381ee';
+const options = {
+  method : "post",
+  auth: "nob:906903ceff56eef301758cfdcb75c998-us1"
+}
+const request = https.request(url, options, function (response) {
+
+  if(response.statusCode === 200){
+    res.sendFile(__dirname + "/success.html");
+  }else{
+    res.sendFile(__dirname + "/failure.html");
+  }
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+});
+
+request.write(jsonData);
+request.end();
+
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function() {
   console.log("running on 3000");
 });
+
+//apikey = 906903ceff56eef301758cfdcb75c998-us1;
+
+//unique id for audience newsletter : d2f55381ee
